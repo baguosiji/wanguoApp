66d7f9c55b8c82b2165d0749a9b38a7a636d686a
512370889 <512370889@qq.com>
2018-04-02 08:25:14 +0800

    我也忘了改了点什么好多

---
+++ js/common.js
@@ -0,0 +1,27 @@
+var http_url = "http://192.168.50.50:81/api.php/Index/index";
+var http_urls="/api.php/Index/index";
+Date.prototype.format = function(fmt) {
+  //author: meizz
+  var o = {
+    "M+": this.getMonth() + 1, //月份
+    "d+": this.getDate(), //日
+    "h+": this.getHours(), //小时
+    "m+": this.getMinutes(), //分
+    "s+": this.getSeconds(), //秒
+    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+    S: this.getMilliseconds() //毫秒
+  };
+  if (/(y+)/.test(fmt))
+    fmt = fmt.replace(
+      RegExp.$1,
+      (this.getFullYear() + "").substr(4 - RegExp.$1.length)
+    );
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt))
+      fmt = fmt.replace(
+        RegExp.$1,
+        RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length)
+      );
+  return fmt;
+};
+var http_time = new Date().format("yyyy-MM-dd hh");
